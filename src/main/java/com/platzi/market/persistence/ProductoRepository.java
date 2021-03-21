f805c55cb9f9c521f60702c41aff8f87c7805d81
Oscar Founes <oscar.founes@gmail.com>
2021-03-21 11:11:37 +0100

    Realizando consultas usando Query Methods

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -4,6 +4,7 @@ import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
@@ -11,5 +12,14 @@ public class ProductoRepository {
     //Recuperarmos todos los productos que hay en nuestra base de datos
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+    //El metodo findByIdCategoria esta relacionado con la lista ubicada en productoCrudRepository
+    //cualquier cambio se tienen que hacer en los dos ficheros.
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoria(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -3,6 +3,22 @@ package com.platzi.market.persistence.crud;
 import com.platzi.market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+    /**
+     * Otra forma de hacer una consulta es de manera nativa:
+     * @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true);
+     * List<Producto> nombreQueQuerramos(int idCategoria);
+     *
+     * siempre es mejor usar los query methods por la flexivibidad que nos ofrece
+     * a la hora de implementar nuestro codigo.
+     */
+    List<Producto> findByIdCategoria(int idCategoria);
+
+    //Nos devuelve los productos con menos conatidad de stock mas vendidos
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 
 }
