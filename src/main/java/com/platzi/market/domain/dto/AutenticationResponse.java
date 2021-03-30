820f69fb99f4eb2c32994c558ce6ecd8d1cb1571
Oscar Founes <oscar.founes@gmail.com>
2021-03-30 11:36:25 +0200

    Creado el metodo que genera nuestro Json Web Token y los Dtos que permiten la autenticacion dentro de nuestra aplicacion

---
+++ src/main/java/com/platzi/market/domain/dto/AutenticationResponse.java
@@ -0,0 +1,17 @@
+package com.platzi.market.domain.dto;
+
+public class AutenticationResponse {
+    private String jwt;
+
+    public AutenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+}
